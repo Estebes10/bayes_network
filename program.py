e4ba6d1a1899fb3fee217f2b6318e386d868df2e
Salomon <salomon.olivera@hotmail.com>
2018-10-13 23:04:33 -0500

    Reader ready

--- program.py
+++ program.py
@@ -1,11 +1,26 @@
-import fileinput
+# -*- coding: utf-8 -*-
 
-def main():
-    lines = []
+# =============================================================================
+# Juan Carlos Estebes González
+# Salomón Olivera Abud
+# =============================================================================
 
-    for line in fileinput.input():
-        lines.append(line)
-        print(line)
+def bayes(nodes, probabilities, queries):
+	return 0
 
-if __name__== "__main__":
-    main()
+if __name__ == "__main__":
+
+	nodes_names = input()
+	number_p = int(input())
+
+	probabilities = []
+	for x in range(0, number_p):
+		probabilities.append(input())
+
+	number_q = int(input())
+	queries = []
+
+	for x in range(0, number_q):
+		queries.append(input())
+
+	bayes(nodes_names, probabilities, queries)
